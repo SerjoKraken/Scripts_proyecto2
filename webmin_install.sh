767b382ccd1211fa2ebaa2691bbd5da11aba5518
serjo <snavarro17@alumnos.utalca.cl>
2022-10-16 20:58:30 -0300

    Se arregla error

--- webmin_install.sh
+++ webmin_install.sh
@@ -2,7 +2,7 @@ sudo apt update
 
 curl -fsSL https://download.webmin.com/jcameron-key.asc | sudo gpg --dearmor -o /usr/share/keyrings/webmin.gpg
 
-sudo echo 'deb [signed-by=/usr/share/keyrings/webmin.gpg] http://download.webmin.com/download/repository sarge contrib' /etc/apt/sources.list
+sudo echo 'deb [signed-by=/usr/share/keyrings/webmin.gpg] http://download.webmin.com/download/repository sarge contrib' >> /etc/apt/sources.list
 
 sudo apt update
 
