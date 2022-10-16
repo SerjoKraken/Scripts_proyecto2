9c8dcb2c2884f1eeff833ae336722d9ec53b0462
serjo <snavarro17@alumnos.utalca.cl>
2022-10-16 20:38:58 -0300

    Scripts para instalar docker y webmin

---
+++ webmin_install.sh
@@ -0,0 +1,9 @@
+sudo apt update
+
+curl -fsSL https://download.webmin.com/jcameron-key.asc | sudo gpg --dearmor -o /usr/share/keyrings/webmin.gpg
+
+sudo echo 'deb [signed-by=/usr/share/keyrings/webmin.gpg] http://download.webmin.com/download/repository sarge contrib' /etc/apt/sources.list
+
+sudo apt update
+
+sudo apt install webmin
